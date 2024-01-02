19912e3053e7c2b61da500f38979c9041d126b10
shanag1801 <shanag0110@gmail.com>
2024-01-03 00:31:01 +0200

    Changed button's text size and created an on click method

--- app/src/main/java/com/example/shaharapp1/MainActivity.java
+++ app/src/main/java/com/example/shaharapp1/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.shaharapp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+    }
+    public void go(View view){
+        btn.setText("Oh, yeah, I’ve been clicked!");
     }
 }
